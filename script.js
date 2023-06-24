2f833f299a901c496e9043a6aa5faa8a98f63541
Sean Connolly <121255377+seanc00@users.noreply.github.com>
2023-06-24 15:24:00 +0100

    comlpleted subtask 2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello?");
+// console.log("hello?");
+
+function getComputerChoice(computerChoice) {
+    let options = ["Rock", "Paper", "Scissors"];
+    let randomiser = Math.floor(Math.random() * options.length);
+
+    computerChoice = options[randomiser];
+    return computerChoice;
+
+}
+
+console.log(getComputerChoice());
