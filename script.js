e82c78cc5ec2852c4484e479acf7459c1bdb6701
Sean Connolly <121255377+seanc00@users.noreply.github.com>
2023-08-05 20:05:13 +0100

    added function to dtermine computer choice

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function paper() {
   let playerChoice = 'Paper'
   console.log(playerChoice);
 }
+
+// randomises computer choice for rock/paper/scissors
+function computerChoice() {
+  let computerChoice = '';
+  let randomNumber = Math.random();
+
+  if (randomNumber < 1/3) {
+    computerChoice = 'Rock';
+  } else if (randomNumber < 2/3 && randomNumber > 1/3) {
+    computerChoice = 'Paper'
+  } else if (randomNumber < 1 && randomNumber > 2/3) {
+    computerChoice = 'Scissors'
+  }
+
+  console.log(computerChoice);
+  return computerChoice;
+}
