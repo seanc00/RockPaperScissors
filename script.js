bb69298ba2a6ace1d31ef5fe1a5e3e6c0e4de286
Sean Connolly <121255377+seanc00@users.noreply.github.com>
2023-08-05 19:55:06 +0100

    made buttons return player choice in console log

---
+++ script.js
@@ -0,0 +1,14 @@
+function rock() {
+  let playerChoice = 'Rock';
+  console.log(playerChoice);
+}
+
+function scissors() {
+  let playerChoice = 'Scissors'
+  console.log(playerChoice);
+}
+
+function paper() {
+  let playerChoice = 'Paper'
+  console.log(playerChoice);
+}
