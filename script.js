0e79fdae9143bac1442d889e4ef654ba13cf33f9
Sean Connolly <121255377+seanc00@users.noreply.github.com>
2023-08-05 20:25:52 +0100

    added same if/elses but for other 2 player moves

--- script.js
+++ script.js
@@ -27,8 +27,26 @@ function playGame(playerChoice) {
   } else if (playerChoice === 'Rock' && computer === 'Scissors') {
     result = 'Win!';
   }
+    
+  // all paper outcomes
+  if (playerChoice === 'Paper' && computer === 'Paper') {
+    result = 'Draw!';
+  } else if (playerChoice === 'Paper' && computer === 'Scissors') {
+    result = 'Loss!';
+  } else if (playerChoice === 'Paper' && computer === 'Rock') {
+    result = 'Win!';
+    }
+      
+  // all rock outcomes
+  if (playerChoice === 'Scissors' && computer === 'Scissors') {
+    result = 'Draw!';
+  } else if (playerChoice === 'Scissors' && computer === 'Rock') {
+    result = 'Loss!';
+  } else if (playerChoice === 'Scissors' && computer === 'Paper') {
+    result = 'Win!';
+  }
 
   console.log(result);
-  //console.log(playerChoice);
-  //console.log(computer);
+  console.log(playerChoice);
+  console.log(computer);
 }
